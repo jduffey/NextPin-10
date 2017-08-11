bea0aec1934a197fa9918fe8591ef91092b1ea47
Jed Duffey <jedduffey@jeds-mbp.attlocal.net>
2017-08-10 23:45:59 -0400

    Refactor; remove pinPad instantiation to @Before

--- test/Tests.java
+++ test/Tests.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,33 +8,35 @@ import static org.junit.Assert.*;
  */
 public class Tests {
 
+    private PinPad pinPad = new PinPad();
+
+    @Before
+    public void setup(){
+        PinPad pinPad = new PinPad();
+    }
+
     @Test
     public void thePinPadClassShouldExist() {
-        PinPad pinPad = new PinPad();
         assertNotNull(pinPad);
     }
 
     @Test
     public void thePinPadClassShouldHaveAMethodThatReturnsTheSecretNumber() {
-        PinPad pinPad = new PinPad();
         assertNotNull(pinPad.returnSecretNumber());
     }
 
     @Test
     public void thePinPadShouldHaveAnArrayList() {
-        PinPad pinPad = new PinPad();
         assertNotNull(pinPad.padNumbers);
     }
 
     @Test
     public void theArrayListIsOfSizeNine() {
-        PinPad pinPad = new PinPad();
         assertEquals(9, pinPad.padNumbers.size());
     }
 
     @Test
     public void theArrayListContainsTheNumbersZeroThroughNine() {
-        PinPad pinPad = new PinPad();
         for (int i = 0; i < 9; i++) {
             assertTrue(pinPad.padNumbers.contains(i));
         }
@@ -41,14 +44,12 @@ public class Tests {
 
     @Test
     public void thePinPadHasGettersOfKeyNumberAndDirectionNumber() {
-        PinPad pinPad = new PinPad();
         assertNotNull(pinPad.getKeyNumber());
         assertNotNull(pinPad.getDirectionNumber());
     }
 
     @Test
     public void thePinPadHasSettersForKeyNumberAndDirectionNumber() {
-        PinPad pinPad = new PinPad();
         pinPad.setKeyNumber(1);
         pinPad.setDirectionNumber(5);
         assertEquals(1, pinPad.getKeyNumber());
@@ -57,7 +58,6 @@ public class Tests {
 
     @Test
     public void pinPadShouldHaveAMethodForShufflingTheNumbersInItsArrayList() {
-        PinPad pinPad = new PinPad();
         pinPad.shuffle();
         assertTrue(pinPad.padNumbers.indexOf(0) != 0 ||
                 pinPad.padNumbers.indexOf(1) != 1 ||
@@ -73,7 +73,6 @@ public class Tests {
 
     @Test
     public void thePinPadShouldHaveAMethodForSettingTheNumbersForTestingPurposes(){
-        PinPad pinPad = new PinPad();
         pinPad.setNumbers(0,1,2,3,4,5,6,7,8);
         for(Integer i = 0; i < 9; i++){
             assertEquals(i, pinPad.padNumbers.get(i));
@@ -82,7 +81,6 @@ public class Tests {
 
     @Test
     public void ifTheDirectionNumberIsInTheCenterThenTheSecretNumberIsJustTheKeyNumber(){
-        PinPad pinPad = new PinPad();
         pinPad.setKeyNumber(1);
         pinPad.setDirectionNumber(5);
         pinPad.setNumbers(0,1,2,3,4,5,6,7,8);
