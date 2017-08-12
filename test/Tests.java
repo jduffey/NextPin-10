b9dee449174879855377358ca04b351217e04470
Jed Duffey <jedduffey@Jeds-MacBook-Pro.local>
2017-08-12 11:57:57 -0400

    FIX; test had been written incorrectly

--- test/Tests.java
+++ test/Tests.java
@@ -83,7 +83,7 @@ public class Tests {
     public void ifTheDirectionNumberIsInTheCenterThenTheSecretNumberIsJustTheKeyNumber() {
         pinPad.setKeyNumber(1);
         pinPad.setDirectionNumber(5);
-        pinPad.setNumbers(0, 1, 2, 3, 4, 5, 6, 7, 8);
+        pinPad.setNumbers(0, 1, 2, 3, 5, 4, 6, 7, 8);
         assertEquals(pinPad.returnSecretNumber(), pinPad.getKeyNumber());
     }
 
