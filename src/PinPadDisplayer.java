206bdea2f1a967bbd69e022e34c6b0ce42cef3eb
Jed Duffey <jedduffey@Jeds-MacBook-Pro.local>
2017-08-19 15:40:45 -0400

    Add shuffle method to displayed pads; add code to help line up numbers when displayed for square sizes under 10 (i.e. no numbers larger than two digits)

--- src/PinPadDisplayer.java
+++ src/PinPadDisplayer.java
@@ -9,13 +9,17 @@ public class PinPadDisplayer {
 
         for(int i = 0; i < HOW_MANY_PADS_TO_DISPLAY; i++){
 
-            PinPad pinPad = new PinPad(3);
+            PinPad pinPad = new PinPad(9);
+            pinPad.shuffle();
 
             for(int row = 0; row < pinPad.getSquareSize(); row++){
 
                 for(int rowElement = 0; rowElement < pinPad.getSquareSize(); rowElement++) {
 
                     int whichElementAreWeOn = rowElement + row*pinPad.getSquareSize();
+                    if(pinPad.padNumbers.get(whichElementAreWeOn)<10){
+                        System.out.print(" "); // Hacky way to get numbers to line up for square sizes under 10 (i.e. no numbers larger than two digits).
+                    }
                     System.out.print(pinPad.padNumbers.get(whichElementAreWeOn) + " ");
 
                 }
